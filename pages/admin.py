10248a03ff01c9724855437f19ecbf315da7c8d4
patrikduch <duchpatrik@icloud.com>
2020-07-29 13:49:26 +0200

    Entrepreneur model

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Enterpreneur
+
+
 # Register your models here.
+admin.site.register(Enterpreneur)
+
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Enterpreneur(models.Model):
+	firstName = models.CharField(max_length=3, blank=False)
+	lastName = models.CharField(max_length=255, blank=False)
+	email = models.EmailField(max_length=255, blank=True)
+	ico = models.CharField(max_length=3, blank=False)
+
+
