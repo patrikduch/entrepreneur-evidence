6c0d64b6bfff1a040be93d72c599813adcdab4cf
patrikduch <duchpatrik@icloud.com>
2020-07-29 14:11:40 +0200

    Forbidden creation new entrepreneurs via Admin portal

--- pages/admin.py
+++ pages/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-
 from .models import Enterpreneur
 
 
+
+class EnterpreneurAdmin(admin.ModelAdmin):
+    
+    model = Enterpreneur
+
+    def has_add_permission(self, request, obj=None):
+        return False
+	
+
+
 # Register your models here.
-admin.site.register(Enterpreneur)
+admin.site.register(Enterpreneur, EnterpreneurAdmin)
 
