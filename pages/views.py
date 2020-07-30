396ee50064576a077b984acbddf36771c00f8950
patrikduch <duchpatrik@icloud.com>
2020-07-30 15:28:41 +0200

    Save form functionality implementation

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import Enterpreneur
 from .forms import EnterpreneurForm, EnterpreneurRawForm
-
 from pages.helpers.AresFetcherHelper import AresFetcherHelper
 
 
@@ -14,11 +13,20 @@ def home(request):
 
 		if my_form.is_valid():
 			print(my_form["ico"])
-			#print(my_form.cleaned_data)
 
 			if AresFetcherHelper.is_ico_valid('09225471'):
 				# now that data is good
 				print("Yes")
+				print(my_form.cleaned_data)
+
+				#Enterpreneur.objects.create(firstName='Patrik', lastName='Duch', email='duchpatrik@icloud.com', ico='09225471')
+				Enterpreneur.objects.create(**my_form.cleaned_data)
+
+
+				# Reset form credential after successfull submition
+				my_form = EnterpreneurRawForm()
+
+
 		else:
 			print(my_form.errors)
 
