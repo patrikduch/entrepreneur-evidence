5896f581647b03a7c134a09cbb5f1f312c771437
patrikduch <duchpatrik@icloud.com>
2020-09-15 20:41:18 +0200

    Task model fix

--- urlparser/models.py
+++ urlparser/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Task(models.Model):
-	title = models.CharField(max_length=255, blank=False)
-	siteName = models.CharField(max_length=255, blank=False)
-	description = models.EmailField(max_length=255, blank=True)
+	title = models.CharField(max_length=85, blank=False)
+	siteName = models.CharField(max_length=85, blank=False)
+	description = models.CharField(max_length=255, blank=True)
 
 	def __str__ (self):
 		return self.siteName
