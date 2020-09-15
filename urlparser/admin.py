3fe0c0b2eca678401658404aa3e99aede9f46ef0
patrikduch <duchpatrik@icloud.com>
2020-09-15 17:04:35 +0200

    Url parser basic setup

--- urlparser/admin.py
+++ urlparser/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    
+    model = Task
+	
 
 # Register your models here.
+
+admin.site.register(Task, TaskAdmin)
--- urlparser/models.py
+++ urlparser/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+	title = models.CharField(max_length=255, blank=False)
+	siteName = models.CharField(max_length=255, blank=False)
+	description = models.EmailField(max_length=255, blank=True)
+
+	def __str__ (self):
+		return self.siteName
